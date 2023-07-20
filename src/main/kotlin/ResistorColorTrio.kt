1d55580ee5e7fac4f3bfab87ddf93b7fb23033f0
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-20 11:13:45 +0300

    First iteration all tests are green

--- src/main/kotlin/ResistorColorTrio.kt
+++ src/main/kotlin/ResistorColorTrio.kt
@@ -1,6 +1,17 @@
+import kotlin.math.pow
+
 object ResistorColorTrio {
 
     fun text(vararg input: Color): String {
-        TODO("Implement this to complete the task")
+        val (firstDigit, secondDigit, numberOfZeroes) = input
+        var result = "${firstDigit.ordinal}${secondDigit.ordinal}".toInt()
+        result *= 10.toDouble().pow(numberOfZeroes.ordinal.toDouble()).toInt()
+        var i = 0
+        while (result > 1_000) {
+            i += 1
+            result /= 1_000
+        }
+        val unit = Unit.values()[i].name.lowercase()
+        return "$result $unit"
     }
 }
